c3dfe7fd850774d877c17eb7b038f8da26f83851
alvaro <alvaro.touzon@gmail.com>
2017-12-18 22:59:28 +0100

    need more functional cut

--- helper/scroll.js
+++ helper/scroll.js
@@ -2,14 +2,10 @@ var scroll = function (elm) {
   this._height = 46;
   return {
     height: function (elm) {
-      var bt = parseInt(getComputedStyle(elm,null).getPropertyValue('border-top-width'), 10);
-      var bb = parseInt(getComputedStyle(elm,null).getPropertyValue('border-bottom-width'), 10)
       this._height = elm.offsetHeight 
     },
     scrollTo: function (position) {
-      
       elm.scrollTop = this._height * position;
-      
     }
 }
 };
--- helper/ul.js
+++ helper/ul.js
@@ -1,10 +1,22 @@
 var ul = function (cont) {
   this._position = 0;
   return {
+    items: function() {
+      var _items = [];
+      for(var i=0; i<cont.length; i++){
+        _items.push(cont[i].innerHTML);
+      }
+      return _items;
+    },
+    
     position: function (position){
       this._position = position || 0;
     },
     exists: function (position) {
+      position = position || this._position;
+      return cont[position] || false;
+    },
+    li: function (position) {
       position = position || this._position;
       return cont[position] || false;
     },
