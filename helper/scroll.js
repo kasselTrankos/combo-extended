393cf28c427029052fceb869a830319053e71f1f
alvaro <alvaro.touzon@gmail.com>
2017-12-19 20:18:23 +0100

    stable by idea

--- helper/scroll.js
+++ helper/scroll.js
@@ -2,7 +2,7 @@ var scroll = function (elm) {
   this._height = 46;
   return {
     height: function (elm) {
-      this._height = 44
+      this._height = 45
     },
     scrollTo: function (position) {
       elm.scrollTop = this._height * position;
